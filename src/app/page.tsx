6d9203e845be60e148d52c0884fdbb1194e9474c
Saood Ahmed <107537712+SaoodAhmed@users.noreply.github.com>
2024-03-18 14:59:03 +0500

    layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,9 +7,9 @@ export default function Home() {
   return (
     <main>
       
-        {/* <NavBar/>
-        <Hero/>  */}
-        <ListFast/>
+        <NavBar/>
+        <Hero/> 
+        {/* <ListFast/> */}
     </main>
   );
 }
